7829dbdf8e232b90d3512f0da32f900022b67f76
Deviant <michaelngo1337@yahoo.com>
2021-05-20 11:48:30 -0700

    moved JS contents into JS file

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+document.getElementById('search').addEventListener('click', event => {
+  event.preventDefault()
+
+  axios.get(`https://kitsu.io/api/edge/anime?filter[text]=${document.getElementById('name').value}`)
+    .then(res => {
+      let anime = res.data.data
+      for (i = 0; i < 10; i++) {
+        let source = anime[i].attributes.posterImage.medium
+        console.log(anime[i])
+        document.getElementById('anime').innerHTML += `
+                <div class="col s12 m4">
+                  <div class="card">
+                    <div class="card-image">
+                      <img src="${source}" alt="${anime[i].attributes.titles.en_jp}">
+                      <span class="card-title">${anime[i].attributes.titles.en_jp} (EN)<br />${anime[i].attributes.titles.ja_jp} (JP)</span>
+                    </div>
+                    <div class="card-content">
+                      <p>Age Rating: ${anime[i].attributes.ageRating}</p>
+                      <p>Rating: ${anime[i].attributes.averageRating}</p>
+                      <p>Status: ${anime[i].attributes.status}</p>
+                      <p>Number of Episodes: ${anime[i].attributes.episodeCount}</p>
+                    </div>
+                  </div>
+                </div>
+            `
+      }
+      document.getElementById('name').value = ''
+    })
+    .catch(err => console.error(err))
+})
